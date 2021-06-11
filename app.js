0799b8eff18445c406c857070057870c82f760f4
Feril <ferilsunu@gmail.com>
2021-06-11 16:07:37 +0400

    initial Commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT_ || 3000
+const port = process.env.PORT || 3000
 const path = require('path')
 const hbs = require('express-handlebars')
 const mongoose = require('mongoose')
